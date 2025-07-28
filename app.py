58a35b855d90cd12edf977e6f3b4de7b2ab4a78c
FabricaClaroTel <Lucas.RezendeBorges@claro.com.br>
2025-07-28 16:50:42 -0300

    app.py

--- app.py
+++ app.py
@@ -6,21 +6,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    # Tela inicial, sem dados, só botões
+    return render_template("index.html")
+
+@app.route("/painel")
+def painel():
     csv_path = os.path.join(os.path.dirname(__file__), "dados.csv")
 
     if not os.path.exists(csv_path):
         return f"Arquivo CSV não encontrado em: {csv_path}"
 
-    # Lê CSV com separador ; e converte as datas
     df = pd.read_csv(csv_path, sep=';', dayfirst=True, parse_dates=['DT_BASE', 'DATA_IMPORT'])
-
-    # Formata as datas para dd/mm/yyyy
     df['DT_BASE'] = df['DT_BASE'].dt.strftime('%d/%m/%Y')
     df['DATA_IMPORT'] = df['DATA_IMPORT'].dt.strftime('%d/%m/%Y')
 
     data = df.to_dict(orient="records")
 
-    return render_template("index.html", data=data)
+    return render_template("painel.html", data=data)
+
+@app.route("/analise_duplicidade")
+def analise_duplicidade():
+    return render_template("analise_duplicidade.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
